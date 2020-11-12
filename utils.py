07d06ab4b63cd8cf1d8861ddfbfdb26c91d841d6
Ivan Kropotov <ivank1995@hotmail.com>
2020-11-12 16:34:58 +0200

    cleaned up

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import cv2
 import face_recognition
 import numpy as np
 
-# TODO: unify the bounding box coords
 
 def get_face_locations_hog(frame):
     """
@@ -33,38 +32,37 @@ def get_face_locations_dnn(frame, net):
     from opencv.
     """
     h, w = frame.shape[:2]
+    # Could also use (104.0, 117.0, 123.0) for mean, unclear which is the correct one
     blob = cv2.dnn.blobFromImage(cv2.resize(
-        frame, (300, 300)), 1.0, (300, 300), (104.0, 117.0, 123.0))
+        frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
     net.setInput(blob)
+
+    # [image_id, class, confidence, left, bottom, right, top]
     face_locations = net.forward()
     res = []
     for i in range(face_locations.shape[2]):
         confidence = face_locations[0, 0, i, 2]
         if confidence > 0.5:
             box = face_locations[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (x, y, x1, y1) = box.astype("int")
-            # Notice the weird permutation
-            tt = (y, x1, y1, x)
-            res.append(tt)
+            (left, bottom, right, top) = box.astype("int")
+            res.append((top, right, bottom, left))
 
     return res
 
-# TODO: make loading more flexible
+
 def load_haar_cascade(model_file):
-    # model_file='haarcascade_frontalface_alt.xml'
     detector = cv2.CascadeClassifier()
     if not detector.load(model_file):
         print('--(!)Error loading face cascade')
 
     return detector
 
+
 def load_cvdnn(model_file, config_file):
-    # model_file = "models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
-    # config_file = "models/deploy.prototxt"
     net = cv2.dnn.readNetFromCaffe(config_file, model_file)
     return net
 
-# TODO: make drawing bbs more general
+
 def draw_bounding_boxes(frame, face_locations):
     for (top, right, bottom, left) in face_locations:
         center_x = (right + left)//2
@@ -74,5 +72,3 @@ def draw_bounding_boxes(frame, face_locations):
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
     return frame
 
-def get_coordinates():
-    raise NotImplementedError("Function not ready yet!")
