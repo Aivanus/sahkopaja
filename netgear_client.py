1cd8b13efeaf9a9266d5a5ce254819f94791d465
Ivan Kropotov <ivank1995@hotmail.com>
2020-11-12 17:03:29 +0200

    scripts for capturing video on Pi and processing it off device in real time

---
+++ netgear_client.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import argparse
+import time
+
+import cv2
+from vidgear.gears import NetGear
+
+import utils
+
+
+def main(args):
+    client_options = {'compression_format': '.jpg',
+                      'compression_param': cv2.IMREAD_COLOR}
+    client = NetGear(address=args.address, port=args.port, protocol='tcp', bidirectional_mode=True,
+                     pattern=1, receive_mode=True, logging=True, **client_options)
+
+    detector = None
+    if args.detection_method == 'dnn':
+        model_file = "models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+        config_file = "models/deploy.prototxt"
+        detector = utils.load_cvdnn(model_file, config_file)
+    elif args.detection_method == 'haar':
+        model_file = 'models/haarcascade_frontalface_alt.xml'
+        detector = utils.load_haar_cascade(model_file)
+
+    # Keep track of FPS
+    frame_counter = 0
+    start_time = None
+
+    coords = None
+
+    while True:
+
+        # receive frames from network
+        data = client.recv(return_data=coords)
+        if start_time is None:
+            start_time = time.time()
+
+        # check for received frame if Nonetype
+        if data is None:
+            break
+
+        _, frame = data
+
+        if args.detection_method == 'dnn':
+            face_locations = utils.get_face_locations_dnn(frame, detector)
+        else:
+            face_locations = utils.get_face_locations_hog(frame)
+
+        coords = utils.get_centers(face_locations)
+
+        frame = utils.draw_bounding_boxes(frame, face_locations)
+
+        if args.show_video:
+            # Show output window
+            if args.track_face and face_locations:
+                (top, right, bottom, left) = face_locations[0]
+                cv2.imshow("Output Frame", frame[bottom:top, left:right])
+            else:
+                cv2.imshow("Output Frame", frame)
+
+            # check for 'q' key if pressed
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+
+            frame_counter += 1
+
+    elapsed_time = time.time() - start_time
+    print(f"avg FPS: {frame_counter/elapsed_time}")
+
+    # close output window
+    cv2.destroyAllWindows()
+
+    # safely close client
+    client.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Detect faces and extract their bounding boxes from a video stream.")
+
+    parser.add_argument('--address', type=str, default='192.168.0.103',
+                        help="The IP adress of the client device, i.e. running this code.")
+    parser.add_argument('--port', type=int, default=12345,
+                        help="The port to listen to.")
+
+    parser.add_argument('--detection_method', type=str, choices=['hog', 'dnn', 'haar'],
+                        default='dnn', help="Method used for face detection.")
+
+    parser.add_argument('--show_video', action='store_false',
+                        help="Show the processed video stream")
+    parser.add_argument('--track_face', action='store_true',
+                        help="Show a cropped face in the video stream.")
+
+    args = parser.parse_args()
+    main(args)
---
+++ netgear_server.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import argparse
+import time
+
+import cv2
+from vidgear.gears import NetGear, PiGear
+
+
+def main(args):
+
+    # Start capturing live Monitor screen frames with default settings
+    # time_delay for warming the camera up
+    stream = PiGear(time_delay=2, rotation=args.rotation, resolution=args.resolution,
+                    framerate=args.framerate, logging=args.logging).start()
+
+    server_options = {'compression_format': '.jpg',
+                      'compression_param': [cv2.IMWRITE_JPEG_QUALITY, args.compression_quality]}
+    server = NetGear(address=args.address, port=args.port, protocol='tcp', bidirectional_mode=True,
+                     pattern=1, logging=False, **server_options)
+
+    # loop over until KeyBoard Interrupted
+    frame_counter = 0
+    start_time = None
+    while True:
+        try:
+            # read frames from stream
+            frame = stream.read()
+            if start_time is None:
+                start_time = time.time()
+
+            # check for frame if Nonetype
+            if frame is None:
+                break
+
+            # send frame to server
+            recv_data = server.send(frame)
+
+            # print data just received from Client
+            if not(recv_data is None):
+                print(recv_data)
+        except KeyboardInterrupt:
+            break
+
+        frame_counter += 1
+
+    # safely close video stream
+    stream.stop()
+
+    # safely close server
+    server.close()
+
+    elapsed_time = time.time() - start_time
+    print(f"sender avg FPS: {frame_counter/elapsed_time}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Raspberry Pi server that sends video stream from PiCamera.")
+
+    parser.add_argument('--address', type=str, default='192.168.0.103',
+                        help="The IP adress of the client device, i.e. running this code.")
+    parser.add_argument('--port', type=int, default=12345,
+                        help="The port to listen to.")
+
+    parser.add_argument('--rotation', type=int, default=0,
+                        help="Rotate the camera output by given degrees.")
+    parser.add_argument('--resolution', type=int, nargs=2, default=[320, 240],
+                        help="The resolution of the camera captures.")
+    parser.add_argument('--framerate', type=int, default=30,
+                        help="The framerate at which the camera will capture.")
+    parser.add_argument('--logging', action='store_true',
+                        help="Allow logging messages.")
+
+    parser.add_argument('--compression_quality', type=int, default=50,
+                        help="The quality of the compressed image (0-100).")
+
+    args = parser.parse_args()
+    main(args)
