53dc921364d54c1107ca779991ec53f1320c157e
lukkaj <72396473+lukkaj@users.noreply.github.com>
2020-12-19 16:46:37 +0200

    Update netgear_server.py

--- netgear_server.py
+++ netgear_server.py
@@ -2,7 +2,7 @@
 
 import argparse
 import time
-
+import serial
 import cv2
 from vidgear.gears import NetGear, PiGear
 
@@ -43,6 +43,37 @@ def main(args):
             # print data just received from Client
             if not(recv_data is None):
                 print(recv_data)
+                # Baudrate, max 115200 bits per seconds communication with arduino mega
+                ser = serial.Serial('/dev/ttyACM0', 115200) 
+                targetCoordinates = recv_data[0]
+                fromArduino = 'C' 
+                while fromArduino == 'C':
+                    if not targetCoordinates:
+                        ser.write('A'.encode()) 
+                        fromArduino = ser.read()
+                    else:
+                        xCoord = targetCoordinates[0]
+                        yCoord = targetCoordinates[1]
+                        if(xCoord > 50):
+                            ser.write('L'.encode())
+                        elif(xCoord < -50):
+                            ser.write('R'.encode())
+                        elif(xCoord):
+                            ser.write('F'.encode())
+                        elif(yCoord):
+                            ser.write('T'.encode())
+                        elif((xCoord > -50) and (xCoord < 50)):
+                            if(yCoord > 150):
+                                ser.write('F'.encode())
+                            elif(yCoord < -150):
+                                ser.write('T'.encode())
+                            else:
+                                time.sleep(0.05)
+                        else:
+                            ser.write('A'.encode())
+
+                    fromArduino = ser.read()
+                    
         except KeyboardInterrupt:
             break
 
